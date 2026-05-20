3f8d1cd5d80f5b3ba6899745c2e938f018ee30e4
Vaibhav Thorat <v.thorat@raytechnologie.com>
2026-05-20 16:34:14 +0530

    changes clear for vercel

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,12 +2,9 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
+
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
@@ -20,6 +17,10 @@ if (!cached) {
 async function connectToDatabase() {
   if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
   }
 
   if (!cached.promise) {
